72a1cec6f31baaa140a13651ea0d5a7d0fc734d2
GustavoSilva26 <lsilvasu@unitru.edu.pe>
2026-06-02 09:45:48 -0500

    Mi primer código en JavaScript y uso de variables

---
+++ app.js
@@ -0,0 +1,13 @@
+// Así escribimos comentarios(notas para mí que mi código ignora)
+let nombre = "Gustavo";
+let edad = 22;
+let pais = "Perú";
+
+console.log("Nombre del estudiante:");
+console.log(nombre);
+
+console.log("Edad:");
+console.log(edad);
+
+console.log("País de origen:");
+console.log(pais);
