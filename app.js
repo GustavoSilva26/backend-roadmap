57253839bdeb8df2c6721f954a9289ae66948348
GustavoSilva26 <lsilvasu@unitru.edu.pe>
2026-06-02 15:27:58 -0500

    Dominando map y filter con datos de ususarios

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
-// Creamos una función que simula traer datos desde internet (tarda 2 segundos)
-function procesarPagoBanco() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ estado: "Aprobado", transaccionId: 98765 });
-    }, 3000);
-  });
-}
+let usuariosDB = [
+  { nombre: "Ana", correo: "ana@mail.com", estaActivo: true },
+  { nombre: "Pedro", correo: "pedro@mail.com", estaActivo: false },
+  { nombre: "Juan", correo: "juan@mail.com", estaActivo: true },
+  { nombre: "Maria", correo: "maria@mail.com", estaActivo: false },
+];
 
-// Para usar 'await' (esperar), la función que la contiene debe tener la palabra 'async'
-async function ejecutarServidor() {
-  console.log("1. Iniciando transacción...");
+let correosActivos = usuariosDB.filter(
+  (usuario) => usuario.estaActivo === true,
+);
 
-  // Con 'await' le decimos: "Espera a que termine esta función antes de pasar a la siguiente línea"
-  let datosPago = await procesarPagoBanco();
-
-  console.log("2. ¡Datos recibidos con éxito!");
-  console.log(
-    "Pago " +
-      datosPago.estado +
-      ". Número de transacción: " +
-      datosPago.transaccionId +
-      ".",
-  );
-}
-
-ejecutarServidor();
-console.log("3. Esperando la confirmación del banco...");
+console.log("--- Los correos de los usuarios con suscripción activa son: ---");
+console.log(correosActivos.map((usuario) => usuario.correo));
