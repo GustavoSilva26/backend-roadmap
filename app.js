050b08bd7b93afddcd203c08417c076fdfff0213
GustavoSilva26 <lsilvasu@unitru.edu.pe>
2026-06-02 09:56:52 -0500

    Aprendiendo tipos de datos y condiciones if-else

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-// Así escribimos comentarios(notas para mí que mi código ignora)
-let nombre = "Gustavo";
-let edad = 22;
-let pais = "Perú";
+let contraseñaCorrecta = "secreto123";
+let contraseñaIngresada = "secreto123";
 
-console.log("Nombre del estudiante:");
-console.log(nombre);
-
-console.log("Edad:");
-console.log(edad);
-
-console.log("País de origen:");
-console.log(pais);
+if (contraseñaCorrecta === contraseñaIngresada) {
+  console.log("Inicio de sesión exitoso.");
+} else {
+  console.log("Contraseña incorrecta. Inténtalo de nuevo.");
+}
