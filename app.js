c510b73f861ac945a8f005d382893dd1bce2a0ba
GustavoSilva26 <lsilvasu@unitru.edu.pe>
2026-06-02 15:56:45 -0500

    Manejo de errores asincrónicos con try catch y promesas rechazaadas

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-let usuariosDB = [
-  { nombre: "Ana", correo: "ana@mail.com", estaActivo: true },
-  { nombre: "Pedro", correo: "pedro@mail.com", estaActivo: false },
-  { nombre: "Juan", correo: "juan@mail.com", estaActivo: true },
-  { nombre: "Maria", correo: "maria@mail.com", estaActivo: false },
-];
+// Función que simula la petición a un servidor que va a fallar tras 1 segundo
+function contactarServidorExterno() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error("No hay conexión a internet (Error 404)"));
+    }, 3000);
+  });
+}
 
-let correosActivos = usuariosDB.filter(
-  (usuario) => usuario.estaActivo === true,
-);
+async function obtenerDatosDeInternet() {
+  try {
+    console.log("Intentando conectar al sevidor externo...");
+    let respuesta = await contactarServidorExterno();
+    console.log("Respuesta del servidor: " + respuesta);
+  } catch (error) {
+    console.log("Error controlado: " + error.message);
+  }
+}
 
-console.log("--- Los correos de los usuarios con suscripción activa son: ---");
-console.log(correosActivos.map((usuario) => usuario.correo));
+obtenerDatosDeInternet();
